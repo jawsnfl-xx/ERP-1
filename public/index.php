dce28d119b70dba48a027acba0a3396bbc16df54
JetAlone <pyrka.marcin@gmail.com>
2013-11-04 22:06:20 +0100

    Dodanie plików ze starego projektu.
    Change-Id: I2b7519bb48bf191a1b1ace81b95520b1dbf0dabc

--- public/index.php
+++ public/index.php
@@ -1,15 +1,10 @@
 <?php
+error_reporting ( 0 );
+ini_set ( 'display_errors', 0 );
 
+require_once ('libray/Smarty-3.1.14/libs/Smarty.class.php');
+require_once ('controller.class.php');
+require_once ('actionsbase.class.php');
 
-	/**
-	 * Opis projektu:
-	 * 
-	 */
-
-	/**
-	 * Opis pliku:
-	 */
-	 
-
-	print 'test1';
-	print 'testujemy ustawienia kodowania plików: UTF-8; żźćżźćńąśąśęłółóżźćąś';
+$controller = new Controller ();
+$controller->dispatch ();
