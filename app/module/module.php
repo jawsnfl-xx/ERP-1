90e3eed6dc3129167098950c18c7e2efb6a27d12
Marcin Pyrka <pyrka.marcin@gmail.com>
2013-12-18 20:18:09 +0100

    Rozpoczęcie obsługi modułów.

---
+++ app/module/module.php
@@ -0,0 +1,24 @@
+<?php
+namespace Module
+{
+
+    /**
+     * 
+     * @author Marcin Pyrka
+     *
+     */
+    class Module extends \Framework\Base
+    {
+
+        /**
+         *
+         * @param unknown $options            
+         */
+        public function __construct ($options = array())
+        {
+            /**
+             */
+            parent::__construct($options);
+        }
+    }
+}
