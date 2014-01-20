9cf8bcad4d2407f857d519e4989e68a24584a479
Marcin Pyrka <pyrka.marcin@gmail.com>
2014-01-20 13:29:37 +0100

    Rozpoczęcie prac nad modułem działań: action

---
+++ app/application/controller/action.php
@@ -0,0 +1,105 @@
+<?php
+namespace Application\Controller
+{
+
+    use Application\Controller as Controller;
+    use Framework\Registry as Registry;
+    use Framework\RequestMethods as RequestMethods;
+
+    /**
+     *
+     * @author Marcin
+     *         @NOTE
+     *         W ten sposób można zapisać podstawę działania kontrolerów.
+     *         Za pomocą wpisów w komentarzach przez deklaracją
+     *         można inicjować kolejność kroków i wymagać dla podnoszenia sie funkcji
+     *        
+     */
+    class Action extends Controller
+    {
+
+        /**
+         * @readwrite
+         */
+        protected $_parameters;
+
+        public function __construct($options)
+        {
+            $this->_parameters = $options['parameters'];
+        }
+
+        /**
+         * @once
+         * @protected
+         */
+        /**
+         * Wykonywana jest inicjalizacja wszystkiego co może być potrzebne.
+         *
+         * @TODO
+         * Włącznie z przekazaniem dalszych danych dla Smatry
+         */
+        public function init()
+        {
+            print 'init';
+        }
+
+        /**
+         * @protected
+         */
+        /**
+         * Wykonywane są wszystkie funkcje związane z kontrolą uwierzytelniania
+         *
+         * @TODO
+         * Budowa oddzielnej klasy autentykacji.
+         * W tym miejscy jedynie jej uruchmienie.
+         */
+        public function authenticate()
+        {
+            print 'authenticate';
+        }
+
+        /**
+         * @once
+         * @protected
+         */
+        public function notify()
+        {
+            print 'notify';
+        }
+
+        /**
+         * @before init, authenticate,
+         * @after notify
+         */
+        public function index()
+        {
+            print 'index';
+        }
+
+        /**
+         * @before init, authenticate,
+         * @after notify
+         *
+         * @NOTE
+         * @TODO
+         * Dodać wartości ref
+         */
+        public function ref()
+        {
+            print 'ref';
+        }
+
+        /**
+         * @before init,
+         * @after notify
+         *
+         * @NOTE
+         * @TODO
+         * Dodać wartości ret
+         */
+        public function ret()
+        {
+            print 'ret';
+        }
+    }
+}
