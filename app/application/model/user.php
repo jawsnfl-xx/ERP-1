5ea9ebb5e68ed371f2926b7a0a63394677ec078f
Marcin Pyrka <pyrka.marcin@gmail.com>
2014-01-21 08:18:52 +0100

    Update user.php

--- app/application/model/user.php
+++ app/application/model/user.php
@@ -1,5 +1,33 @@
 <?php
 
+/**
+ * Plan rozwoju model/user.php
+ * 
+ * 
+-- -----------------------------------------------------
+-- Table `test`.`users`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `test`.`users` ;
+
+CREATE TABLE IF NOT EXISTS `test`.`users` (
+  `id_users` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(45) NULL,
+  `last_name` VARCHAR(45) NULL,
+  `password` VARCHAR(45) NULL,
+  `key_public` VARCHAR(45) NULL,
+  `key_private` VARCHAR(45) NULL,
+  `key_salt` VARCHAR(45) NULL,
+  `live` INT NULL,
+  `deleted` INT NULL,
+  `created` DATETIME NULL,
+  `modified` DATETIME NULL,
+  PRIMARY KEY (`id_users`),
+  INDEX `password` (`password` ASC),
+  UNIQUE INDEX `key_UNIQUE` (`key_public` ASC),
+  UNIQUE INDEX `key_private_UNIQUE` (`key_private` ASC))
+ENGINE = InnoDB;
+ * 
+ */
 class User extends Framework\Model
 {
 
