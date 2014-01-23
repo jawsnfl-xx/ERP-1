d64431b37c9c3050307fac195ca21d25600b9c67
Marcin Pyrka <pyrka.marcin@gmail.com>
2014-01-23 10:02:24 +0100

    Poprawki w bootstrap i controller

--- app/application/controller.php
+++ app/application/controller.php
@@ -24,27 +24,6 @@ namespace Application
             /**
              */
             parent::__construct($options);
-            
-            /**
-             * @TODO:
-             * - nawiązać połączenie z bazą danych;
-             */
-            $this->database = new \Framework\Database();
-            
-            $this->database->_options = array(
-                "options" => array(
-                    "host" => "localhost",
-                    "username" => "root",
-                    "password" => "",
-                    "schema" => "test",
-                    "port" => "3306"
-                )
-            );
-            
-            $this->database->initialize();
-            
-            // Przykład stosowania połączenia z bazą danych MySQL
-            // $data = $database->_mysql->fetch_array('SHOW TABLES');
         }
 
         /**
--- app/bootstrap.php
+++ app/bootstrap.php
@@ -23,6 +23,27 @@ $parsed = $configuration->parse('configuration/default_config');
 $session = new Framework\Session();
 
 /**
+ * @TODO:
+ * - nawiązać połączenie z bazą danych;
+ */
+$database = new \Framework\Database();
+
+$database->_options = array(
+    "options" => array(
+        "host" => "localhost",
+        "username" => "root",
+        "password" => "",
+        "schema" => "test",
+        "port" => "3306"
+    )
+);
+
+$database->initialize();
+
+// Przykład stosowania połączenia z bazą danych MySQL
+// $data = $database->_mysql->fetch_array('SHOW TABLES');
+
+/**
  * Wywołanie Kontrolera dla Application\Controller
  */
 $controller = new Application\Controller();
