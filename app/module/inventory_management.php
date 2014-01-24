6b8c944ddc08a3d4b0b92c5a77297f9e036570f6
Marcin Pyrka <pyrka.marcin@gmail.com>
2014-01-24 13:43:22 +0100

    Rozpoczęcie budowy strunkury modułów
    Signed-off-by: Marcin Pyrka <pyrka.marcin@gmail.com>

---
+++ app/module/inventory_management.php
@@ -0,0 +1,24 @@
+<?php
+namespace Module
+{
+
+    /**
+     *
+     * @author Marcin Pyrka
+     *        
+     */
+    class Inventory_management extends Module
+    {
+
+        /**
+         *
+         * @param unknown $options            
+         */
+        public function __construct($options = array())
+        {
+            /**
+             */
+            parent::__construct($options);
+        }
+    }
+}
