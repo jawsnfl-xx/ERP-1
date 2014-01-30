0655a3ceef6c72d60a703a0e169577933af58bd8
Marcin Pyrka <pyrka.marcin@gmail.com>
2014-01-30 19:12:10 +0100

    Wyłącznie obsługi nowego modułu w home/qm
    Signed-off-by: Marcin Pyrka <pyrka.marcin@gmail.com>

--- app/application/controller/home.php
+++ app/application/controller/home.php
@@ -5,153 +5,147 @@
  * @author Marcin
  *
  */
-namespace Application\Controller
-{
+namespace Application\Controller {
 
-    use Application\Controller as Controller;
-    use Framework\Registry as Registry;
-    use Framework\RequestMethods as RequestMethods;
-    use Framework\View;
+	use Application\Controller as Controller;
+	use Framework\Registry as Registry;
+	use Framework\RequestMethods as RequestMethods;
+	use Framework\View;
 
-    /**
-     *
-     * @author Marcin
-     *        
-     */
-    class Home extends Controller
-    {
+	/**
+	 *
+	 * @author Marcin
+	 *
+	 */
+	class Home extends Controller {
 
-        /**
-         * @readwrite
-         */
-        protected $_parameters;
+		/**
+		 * @readwrite
+		 */
+		protected $_parameters;
 
-        /**
-         * @readwrite
-         */
-        protected $_options;
+		/**
+		 * @readwrite
+		 */
+		protected $_options;
 
-        /**
-         *
-         * @param unknown $options            
-         */
-        public function __construct($options = array())
-        {
-            /**
-             */
-            $this->_parameters = $options['parameters'];
-        }
+		/**
+		 *
+		 * @param unknown $options
+		 */
+		public function __construct($options = array()) {
+			/**
+			 */
+			$this->_parameters = $options ['parameters'];
+		}
 
-        /**
-         * @once
-         * @protected
-         *
-         *
-         * Wykonywana jest inicjalizacja wszystkiego co może być potrzebne.
-         *
-         * @TODO
-         * Włącznie z przekazaniem dalszych danych dla Smatry
-         */
-        public function init()
-        {}
+		/**
+		 * @once
+		 * @protected
+		 *
+		 *
+		 * Wykonywana jest inicjalizacja wszystkiego co może być potrzebne.
+		 *
+		 * @TODO
+		 * Włącznie z przekazaniem dalszych danych dla Smatry
+		 */
+		public function init() {
+		}
 
-        /**
-         * @protected
-         *
-         *
-         * Wykonywane są wszystkie funkcje związane z kontrolą uwierzytelniania
-         *
-         * @TODO
-         * Budowa oddzielnej klasy autentykacji.
-         * W tym miejscy jedynie jej uruchmienie.
-         */
-        public function authenticate()
-        {
-            
-            /**
-             */
-            // $configuration = Registry::get("configuration");
-            // $database = Registry::get("database");
-            $session = Registry::get("session");
-            
-            if (! $session->getup('user')) {
-                header("Location: ?url=users/index");
-                // print 'nie jest zalogowany';
-            }
-            // var_dump($session);
-        }
+		/**
+		 * @protected
+		 *
+		 *
+		 * Wykonywane są wszystkie funkcje związane z kontrolą uwierzytelniania
+		 *
+		 * @TODO
+		 * Budowa oddzielnej klasy autentykacji.
+		 * W tym miejscy jedynie jej uruchmienie.
+		 */
+		public function authenticate() {
 
-        /**
-         * @once
-         * @protected
-         */
-        public function notify()
-        {}
+			/**
+			 */
+			// $configuration = Registry::get("configuration");
+			// $database = Registry::get("database");
+			$session = Registry::get ( "session" );
 
-        /**
-         * @before init, authenticate,
-         * @after notify
-         */
-        public function index()
-        {}
+			if (! $session->getup ( 'user' )) {
+				header ( "Location: ?url=users/index" );
+				// print 'nie jest zalogowany';
+			}
+			// var_dump($session);
+		}
 
-        /**
-         * @before init, authenticate,
-         * @after notify
-         *
-         * @NOTE
-         * Podstrony możliwe dla settings:
-         * - access permissions
-         * - properties
-         * - system settings
-         */
-        public function settings()
-        {
-            /**
-             */
-            if ($this->_parameters[0] === 'access_permissions') {
-                // print 'access permissions';
-            }
-            
-            /**
-             */
-            if ($this->_parameters[0] === 'properties') {
-                // print 'properties';
-            }
-            
-            /**
-             */
-            if ($this->_parameters[0] === 'system settings') {
-                // print 'system_settings';
-            }
-        }
+		/**
+		 * @once
+		 * @protected
+		 */
+		public function notify() {
+		}
 
-        /**
-         * @before init, authenticate,
-         * @after notify
-         *
-         * @NOTE
-         * Podstrony możliwe dla settings:
-         * - access permissions
-         * - properties
-         * - system settings
-         */
-        public function quality_management()
-        {
-            $quality_management = new \Module\Quality_management();
-            
-            // var_dump($production_quality_management);
-            /**
-             */
-            if ($this->_parameters[0] === 'review') {
-                // print 'review';
-            }
-            
-            /**
-             */
-            if ($this->_parameters[0] === 'add') {
-                // print 'add';
-            }
-        }
-    }
+		/**
+		 * @before init, authenticate,
+		 * @after notify
+		 */
+		public function index() {
+		}
+
+		/**
+		 * @before init, authenticate,
+		 * @after notify
+		 *
+		 * @NOTE
+		 * Podstrony możliwe dla settings:
+		 * - access permissions
+		 * - properties
+		 * - system settings
+		 */
+		public function settings() {
+			/**
+			 */
+			if ($this->_parameters [0] === 'access_permissions') {
+				// print 'access permissions';
+			}
+
+			/**
+			 */
+			if ($this->_parameters [0] === 'properties') {
+				// print 'properties';
+			}
+
+			/**
+			 */
+			if ($this->_parameters [0] === 'system settings') {
+				// print 'system_settings';
+			}
+		}
+
+		/**
+		 * @before init, authenticate,
+		 * @after notify
+		 *
+		 * @NOTE
+		 * Podstrony możliwe dla settings:
+		 * - access permissions
+		 * - properties
+		 * - system settings
+		 */
+		public function quality_management() {
+			// $quality_management = new \Module\Quality_management ();
+
+			// var_dump($production_quality_management);
+			/**
+			 */
+			if ($this->_parameters [0] === 'review') {
+				// print 'review';
+			}
+
+			/**
+			 */
+			if ($this->_parameters [0] === 'add') {
+				// print 'add';
+			}
+		}
+	}
 }
