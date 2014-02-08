b65f19cdfc7d85d3e43a93981f172b537818e92b
Marcin Pyrka <pyrka.marcin@gmail.com>
2014-02-08 23:41:16 +0100

    Moduły
    Signed-off-by: Marcin Pyrka <pyrka.marcin@gmail.com>

--- app/module/quality_management.php
+++ app/module/quality_management.php
@@ -5,18 +5,28 @@ namespace Module {
 	/**
 	 *
 	 * @author Marcin Pyrka
-	 *        
+	 *
 	 */
 	class Quality_management extends Module {
-		
+
+		/**
+		 * @readwrite
+		 */
+		public $sheet;
+
 		/**
 		 *
-		 * @param unknown $options        	
+		 * @param unknown $options
 		 */
 		public function __construct($options = array()) {
 			/**
 			 */
 			parent::__construct ( $options );
 		}
+		// public function view_sheet() {
+		// print 'asdasd';
+		// $this->sheet = new \Module\Quality_management\Production_quality_management ();
+		// return $this->sheet;
+		// }
 	}
 }
--- app/module/quality_management/production_quality_management.php
+++ app/module/quality_management/production_quality_management.php
@@ -9,18 +9,19 @@ namespace Module {
 
 	/**
 	 */
+	use Module as Module;
 	use Module\Quality_management as Quality_management;
 
 	/**
 	 *
 	 * @author Marcin Pyrka
-	 *        
+	 *
 	 */
 	class Production_quality_management extends Quality_management {
-		
+
 		/**
 		 *
-		 * @param unknown $options        	
+		 * @param unknown $options
 		 */
 		public function __construct($options = array()) {
 			/**
