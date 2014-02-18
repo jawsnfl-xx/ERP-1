ca590d1291c66b76dd5b3481e7fe4741f2120238
Marcin Pyrka <pyrka.marcin@gmail.com>
2014-02-18 14:21:24 +0100

    Moduł pt
    Signed-off-by: Marcin Pyrka <pyrka.marcin@gmail.com>

--- app/module/product_technology/product.php
+++ app/module/product_technology/product.php
@@ -39,8 +39,59 @@ namespace Module\Product_technology {
 			$data = NULL;
 
 			// var_dump ( $database );
-			$data = $database->_mysql->fetch_array ( 'SELECT * FROM test.products LIMIT 100;' );
+			$data = $database->_mysql->fetch_array ( 'SELECT * FROM products LIMIT 100;' );
 			// var_dump ( $data );
+			return $data;
+		}
+
+		/**
+		 *
+		 * @return unknown
+		 */
+		public function _createListCount() {
+			$database = Registry::get ( "database" );
+			/**
+			 * Wyzerowanie zmienynch
+			 */
+			$count = NULL;
+
+			$count = $database->_mysql->fetch_array ( 'SELECT COUNT(`id_products`) FROM products;' );
+			return $count ['0'] ['COUNT(`id_products`)'];
+		}
+
+		/**
+		 * Zwraca tabelę assoc z pojedyńczą stroną z listy produktów
+		 *
+		 * SELECT COUNT(`id_products`) FROM table_name;
+		 * SELECT * FROM test.products LIMIT 100;
+		 */
+		/**
+		 *
+		 * @param number $_pageNumber
+		 * @param number $_limitAtPage
+		 * @return unknown
+		 */
+		public function _createSoftList($_pageNumber = 1, $_limitAtPage = 20) {
+			$database = Registry::get ( "database" );
+
+			/**
+			 * Wyzerowanie zmienynch
+			 */
+			$data = NULL;
+			$limit_down = NULL;
+			$limit_up = NULL;
+
+			/**
+			 * Obliczenia
+			 */
+			$limit_down = $_limitAtPage * ($_pageNumber - 1);
+			$limit_up = $_limitAtPage;
+
+			/**
+			 * PAMIĘTAJ!!!
+			 * limit działa przyrostowo!
+			 */
+			$data = $database->_mysql->fetch_array ( 'SELECT * FROM products LIMIT ' . $limit_down . ' , ' . $limit_up . ';' );
 			return $data;
 		}
 
@@ -53,7 +104,7 @@ namespace Module\Product_technology {
 			$data = NULL;
 
 			// var_dump ( $database );
-			$data = $database->_mysql->fetch_array ( 'SELECT * FROM test.products WHERE
+			$data = $database->_mysql->fetch_array ( 'SELECT * FROM products WHERE
     `id_products` = ' . $_id . ' LIMIT 100;' );
 			// var_dump ( $data );
 			return $data;
