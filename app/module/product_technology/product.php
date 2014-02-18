8ce4a8b4dc19e93dd5c4867d388f5b4f00142447
Marcin Pyrka <pyrka.marcin@gmail.com>
2014-02-18 07:26:10 +0100

    Moduł qm i pt
    Signed-off-by: Marcin Pyrka <pyrka.marcin@gmail.com>

--- app/module/product_technology/product.php
+++ app/module/product_technology/product.php
@@ -1,24 +1,33 @@
 <?php
 
-namespace Module {
+namespace Module\Product_technology {
 
 	use Module\Product_technology as Product_technology;
+	use Framework\Registry as Registry;
 
 	/**
 	 *
 	 * @author Marcin Pyrka
-	 *        
+	 *
 	 */
 	class Product extends Product_technology {
-		
+
 		/**
 		 *
-		 * @param unknown $options        	
+		 * @param unknown $options
 		 */
 		public function __construct($options = array()) {
 			/**
 			 */
 			parent::__construct ( $options );
+		}
+
+		/**
+		 *
+		 * @param unknown $_id
+		 */
+		public function _isExists($_id) {
+			$database = Registry::get ( "database" );
 		}
 	}
 }
