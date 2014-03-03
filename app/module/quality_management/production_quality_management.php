118102c6266fe149c86830e1532db5b46413897a
Marcin Pyrka <pyrka.marcin@gmail.com>
2014-03-03 12:49:37 +0100

    Update Production_quality_management Signed-off-by: Marcin Pyrka <pyrka.marcin@gmail.com>

--- app/module/quality_management/production_quality_management.php
+++ app/module/quality_management/production_quality_management.php
@@ -18,24 +18,24 @@ namespace Module\Quality_management {
 	/**
 	 *
 	 * @author Marcin Pyrka
-	 *        
+	 *
 	 */
 	class Production_quality_management extends Quality_management {
-		
+
 		/**
 		 * @readwrite
 		 */
 		public $sheet;
 		/**
 		 *
-		 * @param unknown $options        	
+		 * @param unknown $options
 		 */
 		public function __construct($options = array()) {
 			/**
 			 */
 			parent::__construct ( $options );
 		}
-		
+
 		/**
 		 *
 		 * @return NULL
@@ -43,16 +43,37 @@ namespace Module\Quality_management {
 		public function _createList() {
 			$database = Registry::get ( "database" );
 			$data = NULL;
-			
-			$data = $database->_mysql->fetch_array ( 'SELECT * FROM control_measurements LIMIT 100;' );
+
+			$data = $database->_mysql->fetch_array ( 'select
+    *
+from
+    control_measurements
+        left join
+    orders ON control_measurements.orders_id_orders = orders.id_orders
+        left join
+    products ON control_measurements.products_id_products = products.id_products
+        left join
+    packages ON control_measurements.packages_id_packages = packages.id_packages
+LIMIT 100;' );
 			return $data;
 		}
 		public function _createView($_id) {
 			$database = Registry::get ( "database" );
 			$data = NULL;
-			
-			$data = $database->_mysql->fetch_array ( 'SELECT * FROM control_measurements WHERE
-    `id_control_measurements` = ' . $_id . ' LIMIT 1;' );
+
+			$data = $database->_mysql->fetch_array ( 'select
+    *
+from
+    control_measurements
+        left join
+    orders ON control_measurements.orders_id_orders = orders.id_orders
+        left join
+    products ON control_measurements.products_id_products = products.id_products
+        left join
+    packages ON control_measurements.packages_id_packages = packages.id_packages
+where
+    `control_measurements.id_control_measurements` = ' . $_id . '
+LIMIT 1;' );
 			return $data;
 		}
 	}
