24553d290ed2af5a1e1fc55566b15e586e771a56
Marcin Pyrka <pyrka.marcin@gmail.com>
2014-03-04 09:14:00 +0100

    Zmiana app na php
    Signed-off-by: Marcin Pyrka <pyrka.marcin@gmail.com>

---
+++ php/framework/registry.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Framework {
+
+	/**
+	 *
+	 * @author Marcin Pyrka
+	 *        
+	 */
+	class Registry {
+		
+		/**
+		 *
+		 * @var unknown
+		 */
+		private static $_instances = array ();
+		
+		/**
+		 */
+		private function __construct() {
+			// do nothing
+		}
+		
+		/**
+		 */
+		private function __clone() {
+			// do nothing
+		}
+		
+		/**
+		 *
+		 * @param unknown $key        	
+		 * @param string $default        	
+		 * @return \Framework\unknown string
+		 */
+		public static function get($key, $default = null) {
+			if (isset ( self::$_instances [$key] )) {
+				return self::$_instances [$key];
+			}
+			return $default;
+		}
+		
+		/**
+		 *
+		 * @param unknown $key        	
+		 * @param string $instance        	
+		 */
+		public static function set($key, $instance = null) {
+			self::$_instances [$key] = $instance;
+		}
+		
+		/**
+		 *
+		 * @param unknown $key        	
+		 */
+		public static function erase($key) {
+			unset ( self::$_instances [$key] );
+		}
+	}
+}
---
+++ php/framework/requestmethods.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Framework {
+
+	/**
+	 *
+	 * @author Marcin
+	 *        
+	 */
+	class RequestMethods {
+		
+		/**
+		 */
+		private function __construct() {
+			// do nothing
+		}
+		
+		/**
+		 */
+		private function __clone() {
+			// do nothing
+		}
+		
+		/**
+		 *
+		 * @param unknown $key        	
+		 * @param string $default        	
+		 * @return unknown string
+		 */
+		public static function get($key, $default = "") {
+			if (! empty ( $_GET [$key] )) {
+				return $_GET [$key];
+			}
+			return $default;
+		}
+		
+		/**
+		 * @NOTE
+		 * Kasia <3 na to patrzyła.
+		 *
+		 * @param unknown $key        	
+		 * @param string $default        	
+		 * @return unknown string
+		 */
+		public static function post($key, $default = "") {
+			if (! empty ( $_POST [$key] )) {
+				return $_POST [$key];
+			}
+			return $default;
+		}
+		
+		/**
+		 *
+		 * @param unknown $key        	
+		 * @param string $default        	
+		 * @return unknown string
+		 */
+		public static function server($key, $default = "") {
+			if (! empty ( $_SERVER [$key] )) {
+				return $_SERVER [$key];
+			}
+			return $default;
+		}
+	}
+}
