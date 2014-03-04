6d6f8951d869cf7bc0f36cda215c2909c39a5c21
Marcin Pyrka <pyrka.marcin@gmail.com>
2014-03-04 16:51:05 +0100

    Update index.php
    Signed-off-by: Marcin Pyrka <pyrka.marcin@gmail.com>

--- php/index.php
+++ php/index.php
@@ -6,7 +6,7 @@
 define('APP_DIR', dirname(realpath(__FILE__)));
 define('DISPLAY_ERRORS', 1);
 define('MIN_PHP_VERSION', '5.3.7');
-define('DEVELOP_MODE', 1);
+define('DEVELOP_MODE', 0);
 define('BOOTSTRAP_FILE', 'bootstrap.php');
 
 /**
