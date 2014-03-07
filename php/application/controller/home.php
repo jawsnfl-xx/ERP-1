cf18afbe510ebddccf7257d57d31b0e12142447c
Marcin Pyrka <pyrka.marcin@gmail.com>
2014-03-07 11:57:07 +0100

    Update home.php  - pager
    Signed-off-by: Marcin Pyrka <pyrka.marcin@gmail.com>

--- php/application/controller/home.php
+++ php/application/controller/home.php
@@ -234,11 +234,49 @@ namespace Application\Controller
             if ($this->_parameters[0] === 'review') {
                 
                 /**
-                 * Wyświetla listę wszystkich rozpoczętych do tej pory arkuszy kontrolnych
-                 * Dziwnie działa edycja na tablecie.
-                 * :-)
+                 * Sprawdzenie poprawności danych
                  */
-                $this->_table['quality_management']['list'] = $quality_management->_createList();
+                
+                if ($this->_parameters[1] === NULL) {
+                    $page = 1;
+                } else {
+                    $page = $this->_parameters[1];
+                }
+                
+                if ($this->_parameters[2] === NULL) {
+                    $limit = 20;
+                } else {
+                    $limit = $this->_parameters[2];
+                }
+                
+                /**
+                 * Wywołanie tabeli z bazy
+                 */
+                $this->_table['quality_management']['list'] = $quality_management->_createSoftList($page, $limit);
+                
+                /**
+                 * Budowa pagera
+                 *
+                 * UWAGA!
+                 * Napisać na szybko, później przenieść jako uniwersalny element...
+                 */
+                $count = $quality_management->_createListCount();
+                
+                /**
+                 * oblicza ile jest stron pełnych lub napoczętych
+                 */
+                $countPage = ceil($count / $limit);
+                /**
+                 * Teraz powinnien zbudować tablicę, z wartościami dla poszczególych klocków pagiera ;P
+                 */
+                $pager = array();
+                for ($i = 1; $i <= $countPage; $i ++) {
+                    $tmp = array();
+                    $tmp['page'] = $i;
+                    $tmp['limit'] = $limit;
+                    $pager[] = $tmp;
+                }
+                $this->_table['quality_management']['pager'] = $pager;
             } elseif ($this->_parameters[0] === 'view') {
                 /**
                  * Wyświetla arkusz kontrolny z badania
