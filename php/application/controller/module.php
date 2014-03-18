781fcda265f5b448b7a811699e5588da822c6c84
Marcin Pyrka <pyrka.marcin@gmail.com>
2014-03-18 17:39:18 +0100

    Update module.php

--- php/application/controller/module.php
+++ php/application/controller/module.php
@@ -219,6 +219,8 @@ namespace Application\Controller
                         $this->_table['product']['view'] = $session->getup("product/add/return_value");
                         $this->_table['product']['units'] = $session->getup("product/add/name_valueUnits");
                         $this->_table['product']['category'] = $session->getup("product/add/name_valueCategory");
+                        
+                        var_dump($this->_table['product']['view']);
                     } else {
                         /**
                          * Nie wpisano numeru kroku w pasek adresu
