282a9dbf084cccc018fe6cb667ccbf7129a1cd91
Marcin Pyrka <pyrka.marcin@gmail.com>
2014-03-24 08:33:10 +0100

    Update database.php
    Signed-off-by: Marcin Pyrka <pyrka.marcin@gmail.com>

--- php/framework/database.php
+++ php/framework/database.php
@@ -10,7 +10,7 @@ namespace Framework
     /**
      *
      * @author Marcin Pyrka
-     *        
+     *
      */
     class Database extends Base
     {
@@ -32,52 +32,22 @@ namespace Framework
          * @var unknown
          */
         public $_orm;
-        
-        // /**
-        // * (non-PHPdoc)
-        // *
-        // * @see \Framework\Base::_getExceptionForImplementation()
-        // */
-        // protected function _getExceptionForImplementation($method) {
-        // return new Exception\Implementation ( "{$method} method not
-        // implemented" );
-        // }
-        
+
         /**
          */
         public function initialize()
         {
-            
+
             /**
+             * Inicjowanie połączenia z bazą MySQL przez MySQLi
              */
             $this->_mysql = new Database\Mysql();
             $this->_mysql->connect($this->_options);
-            
-            // var_dump($this->_mysql);
-            
-            // var_dump( $this->_mysql );
-            // $this->_mysql->query();
-            // return $dupa;
-            
+
             /**
-             */
-            
-            // $this->_pdo = new Database\PDO();
-            // $this->_pdo->connect($this->_options);
-            
-            // $stmt = $this->_pdo->query('SELECT * FROM products
-            // left join units on units.id_units = products.units_id_units
-            // left join category_product on category_product.product_id_product = products.id_products
-            // left join category on category.id_category = category_product.category_id_category
-            // LIMIT 100;');
-            
-            // var_dump($stmt);
-            
-            /**
+             * Inicjowanie połączenia z bazą MySQL przez DBAL
              */
             $this->_orm = new Database\ORM($this->_options);
-            
-            // var_dump($this);
         }
     }
 }
