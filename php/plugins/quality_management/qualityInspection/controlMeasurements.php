82c5be505a3f37b068c1ce99de2f65b94d1b841d
Marcin Pyrka <pyrka.marcin@gmail.com>
2014-03-25 10:41:11 +0100

    Add controll measurment class

---
+++ php/plugins/quality_management/qualityInspection/controlMeasurements.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ *
+ * @author Marcin
+ *
+ */
+namespace Plugins\Quality_management\QualityInspection
+{
+
+    use Quality_management as Quality_management;
+    use Quality_management\QualityInspection as QualityInspection;
+
+    /**
+     *
+     * @author Marcin Pyrka
+     *        
+     * @name ControlMeasurements
+     * @namespace QualityInspection
+     *           
+     *           
+     *           
+     */
+    class ControlMeasurements extends QualityInspection
+    {
+
+        /**
+         *
+         * @param unknown $options            
+         */
+        public function __construct($options = array())
+        {
+            /**
+             */
+            parent::__construct($options);
+        }
+    }
+}
