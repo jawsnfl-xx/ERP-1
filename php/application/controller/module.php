be3cc1bdbbe6f178cc8c9032fcd2680b6b66af81
Marcin Pyrka <pyrka.marcin@gmail.com>
2014-03-25 12:15:01 +0100

    Update QM

--- php/application/controller/module.php
+++ php/application/controller/module.php
@@ -128,7 +128,7 @@ namespace Application\Controller
         public function quality_management()
         {
             $quality_management = new \Plugins\Quality_management\Production_quality_management();
-            $controlSheet = new \Plugins\Quality_management\QualityInspection\ControlMeasurements\ControlSheet();
+            // $controlSheet = new \Plugins\Quality_management\QualityInspection\ControlMeasurements\ControlSheet();
             if ($this->_parameters[0] === 'review') {
 
                 include_once 'module/quality_management/review/review.php';
