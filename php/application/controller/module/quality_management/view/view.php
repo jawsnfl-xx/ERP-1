77e267745ec39d83340ce65d6af749e6250ab160
Marcin Pyrka <pyrka.marcin@gmail.com>
2014-03-26 08:48:56 +0100

    Update qm view

--- php/application/controller/module/quality_management/view/view.php
+++ php/application/controller/module/quality_management/view/view.php
@@ -2,14 +2,15 @@
 use Application\Controller as Controller;
 use Framework\Registry as Registry;
 use Framework\RequestMethods as RequestMethods;
+use Framework\ArrayMethods as ArrayMethods;
 use Framework\View;
 use Framework\Request;
 
 /**
  * Wyświetla arkusz kontrolny z badania
  */
-$this->_table['quality_management']['view'] = $quality_management->_createView($this->_parameters[1]);
-
+$this->_table['quality_management']['view'] = ArrayMethods::clean($quality_management->_createView($this->_parameters[1]));
+// var_dump($this->_table['quality_management']['view']);
 /**
  * Tymczasowo ładowany widok ControlSheet.
  * Do rozbudowy.
