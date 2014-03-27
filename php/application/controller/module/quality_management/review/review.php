b3e5588b229455c4ef9e8f1e465bc2528cc33c89
Marcin Pyrka <pyrka.marcin@gmail.com>
2014-03-27 08:26:35 +0100

    Delete module empty files
    Signed-off-by: Marcin Pyrka <pyrka.marcin@gmail.com>

--- php/application/controller/module/quality_management/review/review.php
+++
@@ -1,6 +0,0 @@
-<?php
-use Application\Controller as Controller;
-use Framework\Registry as Registry;
-use Framework\RequestMethods as RequestMethods;
-use Framework\View;
-use Framework\Request;
--- php/application/controller/module/quality_management/view/view.php
+++
@@ -1,7 +0,0 @@
-<?php
-use Application\Controller as Controller;
-use Framework\Registry as Registry;
-use Framework\RequestMethods as RequestMethods;
-use Framework\ArrayMethods as ArrayMethods;
-use Framework\View;
-use Framework\Request;
