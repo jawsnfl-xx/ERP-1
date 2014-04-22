464c34f43926e8dd0f206413e87093749eaf21cc
jetalone <pyrka.marcin@gmail.com>
2014-04-22 08:52:26 +0200

    Add sheet, composer update

---
+++ core/framework/sheet.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Framework {
+
+	use Framework\Base as Base;
+
+	/**
+	 *
+	 * @author Marcin 'jetAlone' Pyrka, pyrka.marcin@gmail.com
+	 */
+	class Sheet extends Base {
+
+		/**
+		 */
+		public function initialize() {
+		}
+	}
+}
