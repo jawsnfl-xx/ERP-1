9d863ba04c9ac62026a1618eba119ca1092f47c7
jetalone <pyrka.marcin@gmail.com>
2014-04-22 10:07:01 +0200

    Update

--- core/framework/base.php
+++ core/framework/base.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Framework {
 
 	use Framework\Inspector as Inspector;
@@ -11,109 +12,104 @@ namespace Framework {
 	 * @author Marcin 'jetAlone' Pyrka, pyrka.marcin@gmail.com
 	 */
 	class Base {
-
-		/**
-		 *
-		 * @var unknown
-		 */
 		private $_inspector;
 
 		/**
 		 *
-		 * @param array $options        	
+		 * @param array $options
 		 */
 		public function __construct($options = array()) {
-			$this->_inspector = new Inspector($this);
-			if (is_array($options) || is_object($options)) {
-				foreach ($options as $key => $value) {
-					$key = ucfirst($key);
+			$this->_inspector = new Inspector ( $this );
+			if (is_array ( $options ) || is_object ( $options )) {
+				foreach ( $options as $key => $value ) {
+					$key = ucfirst ( $key );
 					$method = "set{$key}";
-					$this->$method($value);
+					$this->$method ( $value );
 				}
 			}
 		}
 
 		/**
 		 *
-		 * @param unknown $name        	
-		 * @param unknown $arguments        	
+		 * @param unknown $name
+		 * @param unknown $arguments
 		 * @throws Exception
 		 * @return NULL \Framework\Base
 		 */
 		public function __call($name, $arguments) {
-			if (empty($this->_inspector)) {
-				throw new Exception("Call parent::__construct!");
+			if (empty ( $this->_inspector )) {
+				throw new Exception ( "Call parent::__construct!" );
 			}
-			
+
 			/**
 			 * @TODO: - opisać działanie $getMatches - ------||------ $setMatches
 			 */
-			$getMatches = StringMethods::match($name, "^get([a-zA-Z0-9]+)$");
-			if (sizeof($getMatches) > 0) {
-				$normalized = lcfirst($getMatches[0]);
+			$getMatches = StringMethods::match ( $name, "^get([a-zA-Z0-9]+)$" );
+			if (sizeof ( $getMatches ) > 0) {
+				$normalized = lcfirst ( $getMatches [0] );
 				$property = "_{$normalized}";
-				if (property_exists($this, $property)) {
-					$meta = $this->_inspector->getPropertyMeta($property);
-					if (empty($meta["@readwrite"]) && empty($meta["@read"])) {
-						throw $this->_getExceptionForWriteonly($normalized);
+				if (property_exists ( $this, $property )) {
+					$meta = $this->_inspector->getPropertyMeta ( $property );
+					if (empty ( $meta ["@readwrite"] ) && empty ( $meta ["@read"] )) {
+						throw $this->_getExceptionForWriteonly ( $normalized );
 					}
-					if (isset($this->$property)) {
+					if (isset ( $this->$property )) {
 						return $this->$property;
 					}
 					return null;
 				}
 			}
-			$setMatches = StringMethods::match($name, "^set([a-zA-Z0-9]+)$");
-			if (sizeof($setMatches) > 0) {
-				$normalized = lcfirst($setMatches[0]);
+			$setMatches = StringMethods::match ( $name, "^set([a-zA-Z0-9]+)$" );
+			if (sizeof ( $setMatches ) > 0) {
+				$normalized = lcfirst ( $setMatches [0] );
 				$property = "_{$normalized}";
-				if (property_exists($this, $property)) {
-					$meta = $this->_inspector->getPropertyMeta($property);
-					if (empty($meta["@readwrite"]) && empty($meta["@write"])) {
-						throw $this->_getExceptionForReadonly($normalized);
+				if (property_exists ( $this, $property )) {
+					$meta = $this->_inspector->getPropertyMeta ( $property );
+					if (empty ( $meta ["@readwrite"] ) && empty ( $meta ["@write"] )) {
+						throw $this->_getExceptionForReadonly ( $normalized );
 					}
-					$this->$property = $arguments[0];
+					$this->$property = $arguments [0];
 					return $this;
 				}
 			}
-			throw $this->_getExceptionForImplementation($name);
+			throw $this->_getExceptionForImplementation ( $name );
 		}
 
 		/**
 		 *
-		 * @param unknown $name        	
+		 * @param unknown $name
 		 */
 		public function __get($name) {
-			$function = "get" . ucfirst($name);
-			return $this->$function();
+			$function = "get" . ucfirst ( $name );
+			return $this->$function ();
 		}
 
 		/**
 		 *
-		 * @param unknown $name        	
-		 * @param unknown $value        	
+		 * @param unknown $name
+		 * @param unknown $value
 		 */
 		public function __set($name, $value) {
-			$function = "set" . ucfirst($name);
-			return $this->$function($value);
+			$function = "set" . ucfirst ( $name );
+			return $this->$function ( $value );
 		}
 
 		/**
 		 *
-		 * @param unknown $property        	
+		 * @param unknown $property
 		 * @return \Framework\Core\Exception\ReadOnly
 		 */
 		protected function _getExceptionForReadonly($property) {
-			return new Exception\ReadOnly("{$property} is read-only");
+			return new Exception\ReadOnly ( "{$property} is read-only" );
 		}
 
 		/**
 		 *
-		 * @param unknown $property        	
+		 * @param unknown $property
 		 * @return \Framework\Core\Exception\WriteOnly
 		 */
 		protected function _getExceptionForWriteonly($property) {
-			return new Exception\WriteOnly("{$property} is write-only");
+			return new Exception\WriteOnly ( "{$property} is write-only" );
 		}
 
 		/**
@@ -121,16 +117,16 @@ namespace Framework {
 		 * @return \Framework\Core\Exception\Property
 		 */
 		protected function _getExceptionForProperty() {
-			return new Exception\Property("Invalid property");
+			return new Exception\Property ( "Invalid property" );
 		}
 
 		/**
 		 *
-		 * @param unknown $method        	
+		 * @param unknown $method
 		 * @return \Framework\Core\Exception\Argument
 		 */
 		protected function _getExceptionForImplementation($method) {
-			return new Exception\Argument("{$method} method not implemented");
+			return new Exception\Argument ( "{$method} method not implemented" );
 		}
 	}
 }
--- core/framework/sheet.php
+++ core/framework/sheet.php
@@ -4,14 +4,32 @@ namespace Framework {
 
 	use Framework\Base as Base;
 
-	/**
-	 *
-	 * @author Marcin 'jetAlone' Pyrka, pyrka.marcin@gmail.com
-	 */
 	class Sheet extends Base {
 
 		/**
+		 * @readwrite
 		 */
+		protected $_array;
+
+		/**
+		 * @readwrite
+		 */
+		protected $_class;
+
+		/**
+		 * @readwrite
+		 */
+		protected $_parametrs;
+
+		/**
+		 *
+		 * @param unknown $options
+		 */
+		public function __construct($options = array()) {
+			parent::__construct ( $options );
+		}
+		public function __destruct() {
+		}
 		public function initialize() {
 		}
 	}
